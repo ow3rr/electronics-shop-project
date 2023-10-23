f58cecd6f3517fb15efcca9bc8d6f91804c054b0
ow3rr <lighteckiy@ya.ru>
2023-10-23 10:54:17 +0300

    Fix: 1) protected attribute name 2) comments 3) PEP in test_item.py
    Add:
    1)added attribute all to init

--- src/item.py
+++ src/item.py
@@ -1,4 +1,6 @@
 import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -7,9 +9,6 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        self.name = name
-        self.price = price
-        self.quantity = quantity
         """
         Создание экземпляра класса item.
 
@@ -17,54 +16,53 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
+        self._name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
     @property
     def name(self):
+        """- атрибут `name` сделать приватным"""
         return self._name
 
     @name.setter
     def name(self, meter):
+        """- добавить геттер и сеттер для `name`, используя @property
+        - в сеттере `name` проверять, что длина наименования товара не больше 10 симвовов. В противном случае, обрезать строку (оставить первые 10 символов).
+        """
         if len(meter) < 10:
             self._name = meter
         else:
             self._name = meter[:10]
 
-    """атрибут `name` сделать приватным
-    добавить геттер и сеттер для `name`, используя @property
-    в сеттере `name` проверять, что длина наименования товара не больше 10 симвовов,
-    в противном случае, обрезать строку (оставить первые 10 символов)."""
-
     @classmethod
     def instantiate_from_csv(cls, filename):
+        """`instantiate_from_csv()` - класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_
+    """
         cls.all = []
         with open(filename, 'r') as file:
             reader = csv.DictReader(file)
             for i in reader:
-                item = cls(i["name"], i["price"], i["quantity"])
-                cls.all.append(item)
-            return cls.all
-    """`instantiate_from_csv()` - класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_
-"""
+                cls(i["name"], i["price"], i["quantity"])
 
     @staticmethod
     def string_to_number(str_num):
+        """`string_to_number()` - статический метод, возвращающий число из числа-строки"""
         return int(float(str_num))
-    """`string_to_number()` - статический метод, возвращающий число из числа-строки"""
 
     def calculate_total_price(self) -> float:
+        """
+        Рассчитывает общую стоимость конкретного товара в магазине.
+
+        :return: Общая стоимость товара.
+        """
         total_price = self.price * self.quantity
         return total_price
 
-    """
-    Рассчитывает общую стоимость конкретного товара в магазине.
-
-    :return: Общая стоимость товара.
-    """
-
     def apply_discount(self) -> None:
+        """
+        Применяет установленную скидку для конкретного товара.
+        """
         self.price *= self.pay_rate
         return self.price
-
-    """
-    Применяет установленную скидку для конкретного товара.
-    """
