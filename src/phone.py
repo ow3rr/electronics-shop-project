f6168014f47e34de85095e5cbf86dc00a7f82434
ow3rr <lighteckiy@ya.ru>
2023-11-03 20:03:23 +0300

    HW 4 FIX: 1) fix fun add and str in phone 2) fix number_of_sim in phone 3) created test_number_of_sim

--- src/phone.py
+++ src/phone.py
@@ -4,15 +4,10 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.__number_of_sim = number_of_sim
-
-    def __add__(self, other):
-        if not isinstance(other, (Phone, Item)):
-            raise ValueError("Нельзя сложить Item с экземпляром не Phone или Item класса.")
-        return self.quantity + other.quantity
-
-    def __str__(self):
-        return f"{self.name}"
+        if number_of_sim > 0:
+            self.__number_of_sim = number_of_sim
+        else:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
--- tests/test_item.py
+++ tests/test_item.py
@@ -32,31 +32,45 @@ def test_repr():
     item1 = Item("Смартфон", 10000, 20)
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
 
+
 def test_str():
     item1 = Item("Смартфон", 10000, 20)
     assert str(item1) == 'Смартфон'
 
+
 def test_class_phone():
     assert Phone("iPhone 14", 120_000, 5, 2)
+
 
 def test_str_phone():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert str(phone1) == 'iPhone 14'
 
+
 def test_repr_phone():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert repr(phone1) == "Phone('iPhone 14', 120000, 5, 2)"
 
+
 def test_sim_above_zero():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     assert phone1.number_of_sim == 2
+
 
 def test_add_quantity_on_item():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     item1 = Item("Смартфон", 10000, 20)
     assert item1 + phone1 == 25
 
+
 def test_add_quantity_on_phone():
     phone1 = Phone("iPhone 14", 120_000, 5, 2)
     item1 = Item("Смартфон", 10000, 20)
-    assert phone1 + phone1 == 10
+    assert phone1 + item1 == 25
+
+
+def test_number_of_sim():
+    phone1 = Phone("iPhone 14", 120_000, 5, 2)
+    assert phone1.number_of_sim == 2
+    with pytest.raises(ValueError):
+        phone1.number_of_sim = 0
